c5fcde95e26bc152a99518d0eddfdd7dabc6cc97
Dee Race <deerace12@gmail.com>
2018-07-16 16:45:52 -0400

    added functionality to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,17 @@ class UsersController < ApplicationController
     before_action :set_user, only: [:show]
     
     def new 
-
+        @user = User.new
     end 
 
     def create 
-
+        @user = User.new(user_params)
+        if @user.save
+            sessions[:user_id] = @user.id 
+            redirect_to user_path(@user)
+        else 
+            render :new 
+        end 
     end 
 
     def edit 
@@ -33,6 +39,10 @@ class UsersController < ApplicationController
 
     def set_user
         @user = user.find(params[:id])
+    end 
+
+    def user_params
+        params.require(:user).permit(:name, :password_digest, :email)
     end 
 
 
