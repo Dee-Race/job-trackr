6381da8540c1b08c92b9998824082104eecec415
Dee Race <deerace12@gmail.com>
2018-08-27 16:49:00 -0400

    protoype method and notes added

--- app/assets/javascripts/applications.js
+++ app/assets/javascripts/applications.js
@@ -1,6 +1,12 @@
-$(() => {
+// Send a recieve info from the backend without a page reload
+
+
+// .ready() method waits until the browser finishes rendering the HTML doc before triggering callback function
+$(document).ready(() => {
     attachListeners()
-})
+});
+
+// Intercept normal routing by the user - rewriting what happends when user clicks applications
 
 const attachListeners = function() {
     $('.all_apps').on('click', function(event) {
@@ -11,7 +17,8 @@ const attachListeners = function() {
                 $('#app-container').html('')
                 applications.forEach(application => {
                     let newApplication = new Application(application)
-                    console.log(newApplication)
+                    let applicationHtml = newApplication.formatIndex()
+                    $('#app-container').append(applicationHtml)
                 })
             })
     })
@@ -26,4 +33,11 @@ function Application(application) {
     this.job_salary = application.job_salary 
     this.date_applied = application.date_applied
     this.job_url = application.job_url 
+}
+
+Application.prototype.formatIndex = function() {
+    let applicationHtml = `
+        <h1>${this.company}</h1>
+    `
+    return applicationHtml 
 }
